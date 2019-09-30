3891b5eafd02ac9fa3356c9e4a3655b5dbd07e9b
pabu18 <42177203+pabu18@users.noreply.github.com>
2019-10-01 02:00:20 +0530

    Update NumbersActivity.java

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    @Override
+    @Overrideas
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
